82be191191b0a78e6ff95f2280df9e4d9267efaf
Daniel <danisolo91@gmail.com>
2021-09-20 20:19:57 +0200

    add Spring project

---
+++ src/main/java/com/ds/recetasapp/RecetasAppApplication.java
@@ -0,0 +1,13 @@
+package com.ds.recetasapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecetasAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecetasAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ds/recetasapp/RecetasAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.recetasapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecetasAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
