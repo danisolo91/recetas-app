6e3c45876ac739296de6a30af11141ff01cf1a5f
Daniel <danisolo91@gmail.com>
2021-10-28 22:47:22 +0200

    Implement update profile image

--- backend-api/src/main/java/com/ds/recetasapp/controller/UserController.java
+++ backend-api/src/main/java/com/ds/recetasapp/controller/UserController.java
@@ -1,20 +1,27 @@
 package com.ds.recetasapp.controller;
 
+import java.security.Principal;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ds.recetasapp.domain.User;
+import com.ds.recetasapp.payload.response.MessageResponse;
+import com.ds.recetasapp.service.FileStorageService;
 import com.ds.recetasapp.service.RecipeService;
 import com.ds.recetasapp.service.UserService;
 
@@ -25,9 +32,12 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private RecipeService recipeService;
+	
+	@Autowired
+	private FileStorageService fileStorageService;
 
 	@GetMapping
 	@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
@@ -47,14 +57,47 @@ public class UserController {
 	}
 
 	@GetMapping("/{id}/recipes")
-	public ResponseEntity<?> getUserRecipes(@PathVariable UUID id, 
+	public ResponseEntity<?> getUserRecipes(@PathVariable UUID id,
 			@PageableDefault(page = 0, size = 2) Pageable pageable) {
 		Optional<User> user = userService.getUserById(id);
 
 		if (user.isEmpty()) {
 			return ResponseEntity.notFound().build();
 		}
-		
+
 		return ResponseEntity.ok(recipeService.getByAuthorId(user.get().getId(), pageable));
+	}
+
+	@PutMapping("/{id}/profile-image")
+	public ResponseEntity<?> updateProfileImage(@PathVariable UUID id,
+			@RequestParam("profileImage") MultipartFile profileImage, Principal principal) {
+
+		Optional<User> user = userService.getUserById(id);
+
+		// Check if user exists
+		if (user.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		// Check if profile belongs to the authenticated user
+		if(!user.get().getUsername().equals(principal.getName())) {
+			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+		}
+		
+		// Check if profileImage file is an image
+		if(!profileImage.getContentType().startsWith("image/")) {
+			return ResponseEntity.badRequest().body(new MessageResponse("El fichero subido no es una imagen"));
+		}
+		
+		// Store the image
+		String fileName = fileStorageService.storeFile(profileImage);
+		
+		// TODO: Delete current profile image if exists...
+		
+		// Update the user
+		user.get().setProfileImage(fileName);
+		userService.save(user.get());
+		
+		return ResponseEntity.ok(user.get());
 	}
 }
